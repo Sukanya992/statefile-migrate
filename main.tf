d03f6c6902cd5e9bd010961f794dc7baf58ee073
Sukanya992 <sukanya09200206@gmail.com>
2025-05-28 12:38:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project     = "plated-epigram-452709-h6"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "harness_vm" {
+  name         = "harness-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  metadata = {
+    startup-script = <<-EOT
+      #!/bin/bash
+      sudo apt update
+      sudo apt install -y apache2
+      sudo systemctl start apache2
+    EOT
+  }
+
+  tags = ["web"]
+}
